fc87c30d020e4d929f85192d2d09eb569c11bd74
jack <jack.murning@bbc.co.uk>
2019-08-22 15:01:00 +0100

    add body parser

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get("/", (req, res) => res.send("Hello World!"));
 app.get("/get-awesome-things", (req, res) =>
   res.send({
@@ -12,7 +14,7 @@ app.post("/interaction", (req, res) => {
   console.log(req);
   res.send();
 });
-app.post("/command", (req, res) => {
+app.post("/command", , (req, res) => {
   console.log(req.body);
   res.send();
 });
